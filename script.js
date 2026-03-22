1b89892246c15515d9757611c926606fdcf60e7e
Giovany Freitas <251627925+devgfreitas@users.noreply.github.com>
2026-03-22 20:30:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let visor = document.getElementById('visor');
+
+function btnNumber(valor) {
+    visor.value += valor;
+}
+
+function btnBack() {
+    visor.value = visor.value.slice(0, -1);
+}
+
+function btnOperator(op) {
+    visor.value += op;
+}
+
+function percent() {
+    let porcentagem = visor.value;
+    porcentagem = porcentagem.slice(0, 10000000);
+    try {
+        porcentagem = porcentagem / 100;
+        visor.value = porcentagem; 
+    } catch {
+        visor.value = "Error in calculation";
+    }
+    
+}
+
+function clean() {
+    visor.value = ""
+}
+
+function result() {
+    try {
+        let resultado = eval(visor.value);
+        visor.value = resultado;
+    } catch {
+        visor.value = "Error in calculation";
+    }
+    
+}
