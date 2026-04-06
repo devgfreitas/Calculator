632c7e845e48410900a0a404fa8f6f8f58ace364
Giovany <gvnyfreitas@gmail.com>
2026-04-06 11:01:49 -0300

    adicionando eventos de keydown e melhorando layout/design

--- script.js
+++ script.js
@@ -2,19 +2,30 @@ let visor = document.getElementById('visor');
 
 function digit(valor) {
     visor.value += valor;
-}
+};
 
 function btnBack() {
     visor.value = visor.value.slice(0, -1);
-}
+};
 
 function clean() {
     visor.value = ""
-}
+};
+
+document.addEventListener('keydown', function(event) {
+    if(event.key == '0' || event.key == '1' || event.key == '2' || event.key == '3' || event.key == '4' || event.key == '5' || event.key == '6' || event.key == '7' || event.key == '8' || event.key == '9' || event.key == '+' || event.key == '-' || event.key == '*' || event.key == '/' || event.key == '%' || event.key == '.' ) {
+        digit(event.key);
+    } else if(event.key == 'Backspace') {
+        btnBack();
+    } else if (event.key == 'Enter') {
+        resolution();
+    }
+})
 
 function separate() {
     let dadostela = visor.value;
-    return dadostela.match(/\d+|\+|\-|\*|\/|\%/g);
+    return dadostela.match(/\d+(?:[.,]\d+)?|\+|\-|\*|\/|\%/g);
+
 }
 
 function correctAccount() {
